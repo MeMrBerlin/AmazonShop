559ec58115c98b1fecf4efdc2ec663e10bcb8f4e
MeMrBerlin <nishanrkm@gmail.com>
2024-12-12 17:57:55 +0530

    first commit

---
+++ js/amazon.js
@@ -0,0 +1,97 @@
+import { cart, addToCartFunc } from "./cart.js";
+import { products } from "./products.js";
+
+let productHtml = "";
+products.forEach((product) => {
+  productHtml += `<div class="product-container">
+          <div class="product-image-container">
+            <img
+              class="product-image"
+              src="${product.image}"
+            />
+          </div>
+
+          <div class="product-name limit-text-to-2-lines">
+            <span>${product.name}</span>
+          </div>
+
+          <div class="product-rating-container">
+            <img
+              class="product-rating-stars"
+              src="images/ratings/rating-${product.rating.stars * 10}.png"
+            />
+            <div class="product-rating-count link-primary">${
+              product.rating.count
+            }</div>
+          </div>
+
+          <div class="product-price"> &#8377 ${(
+            (product.priceCents / 100) *
+            84.86
+          ).toFixed(2)}</div>
+
+          <div class="product-quantity-container">
+            <select>
+              <option selected value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+
+          <div class="product-spacer"></div>
+
+          <div class="added-to-cart">
+            <img src="images/icons/checkmark.png" />
+            Added
+          </div>
+
+          <button class="add-to-cart-button button-primary js-add-to-cart"
+          data-product-id = "${product.id}"><span>
+          Add to Cart</span></button>
+        </div>
+`;
+});
+
+console.log(productHtml);
+document.querySelector(".js-products-grid").innerHTML = productHtml;
+
+//! update cart quantity function
+function updateCartQuantity() {
+  let totalQuantity = 0;
+  cart.forEach((cartItem) => {
+    totalQuantity += cartItem.quantity;
+  });
+
+  document.querySelector(".js-cart-quantity").innerHTML = totalQuantity;
+}
+document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+  button.addEventListener("click", () => {
+    const productId = button.dataset.productId;
+    const quantity = button
+      .closest(".product-container")
+      .querySelector(".product-quantity-container select").value;
+
+    addToCartFunc(quantity, productId);
+    updateCartQuantity();
+  });
+});
+
+document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+  button.addEventListener("click", () => {
+    const addToCart = button.previousElementSibling;
+    if (addToCart) {
+      addToCart.style.opacity = "1";
+      addToCart.classList.remove("fade-out");
+      setTimeout(() => {
+        addToCart.classList.add("fade-out");
+      }, 700);
+    }
+  });
+});
---
+++ js/cart.js
@@ -0,0 +1,75 @@
+export let cart = [];
+try {
+  const storedCart = JSON.parse(localStorage.getItem("cart"));
+  if (Array.isArray(storedCart)) {
+    cart = storedCart;
+  }
+} catch (e) {
+  console.error("Error parsing cart from localStorage:", e);
+}
+if (!cart.length) {
+  cart = [
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2,
+      deliveryOptionId: "1",
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1,
+      deliveryOptionId: "2",
+    },
+  ];
+  saveToStorage();
+}
+
+//! function to save cart to localStorage
+function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+//! add to cart function
+export function addToCartFunc(quantity, productId) {
+  let matchingItem;
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  if (matchingItem) {
+    matchingItem.quantity += parseInt(quantity);
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: parseInt(quantity),
+      deliveryOptionId: "1",
+    });
+  }
+  saveToStorage();
+}
+
+//! function to delete items from cart
+
+export function removeFromCart(productId) {
+  const newCart = [];
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+  cart = newCart;
+  saveToStorage();
+}
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem;
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
+  saveToStorage();
+}
