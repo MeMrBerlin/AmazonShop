061b42b4cb449b2502d20fe177cae40f02cc6236
MeMrBerlin <nishanrkm@gmail.com>
2024-12-16 17:49:33 +0530

    convert products into classes

--- js/checkout.js
+++ js/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import "../js/cart-class.js";
+// import "../js/cart-class.js";
 
 renderOrderSummary();
 renderPaymentSummary();
